00e318010fcb7135a1e92565693c08d59c177b74
indicozy <berkutkarlibai@gmail.com>
2022-08-08 01:45:43 +0600

    start

---
+++ generate.py
@@ -0,0 +1,13 @@
+from nu_bot.modules.db import add_category, create_tables
+
+import yaml
+
+def run():
+    with open("config.yaml", "r") as stream:
+        category_names = yaml.safe_load(stream)
+        for category_name in category_names['categories']:
+          add_category(category_name)
+
+if __name__ == '__main__':
+    create_tables()
+    run()
---
+++ readme.md
@@ -0,0 +1,24 @@
+# Table
+
+- User
+  - Telegram id
+  - Telegram username
+  - Phone number
+
+- Product
+  - Seller id
+  - Buyer id
+  - Archived boolean
+  - Sold boolean
+
+  - Name
+  - Description
+  - Category id
+  - subcategory id
+
+- Product category
+  - Name
+- product subcategory
+  - Name
+  - category id
+
