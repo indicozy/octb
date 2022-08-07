2f54a297c420d0aeb63cb993306118cbe8998e1a
indicozy <berkutkarlibai@gmail.com>
2022-08-08 03:15:54 +0600

    start

--- octb/__init__.py
+++ octb/__init__.py
@@ -1,5 +1,6 @@
 import logging
 import sys
+from telegram.ext import ApplicationBuilder
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -11,7 +12,10 @@ if sys.version_info[0] < 3 or sys.version_info[1] < 10:
     LOGGER.error("You MUST have a python version of at least 3.10! Multiple features depend on this. Bot quitting.")
     quit(1)
 
+
 # env
+from decouple import config
+
 TOKEN=config('TOKEN')
 
 # main
---
+++ octb/__main__.py
@@ -0,0 +1,37 @@
+# environment
+from decouple import config
+
+# telegram bot
+from telegram import Update, ReplyKeyboardRemove, ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, filters, ConversationHandler, MessageHandler
+
+from octb import application
+
+# from octb.modules.sql.base import Session, engine
+# import octb.modules.sql.base as db
+# from octb.modules import private, products
+
+# def add_all_handlers(application):
+#     # handlers
+#     private_handlers = [
+#             private.startHandler,
+#             products.add_product,
+#             products.edit_handler,
+#         ]
+
+#     all_handlers = [
+#            private_handlers 
+#         ]
+
+#     for handler_group in all_handlers:
+#         for handler in handler_group:
+#             application.add_handler(handler)
+#     application.add_handler(private.handler_private_default)
+
+def main():
+    application.run_polling()
+    # add_all_handlers(application)
+
+if __name__ == '__main__':
+    # db.create_tables()
+    main()
