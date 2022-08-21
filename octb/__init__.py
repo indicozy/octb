e7ab64c2f4aa1ccd63031928af292c87da1b28dc
indicozy <berkutkarlibai@gmail.com>
2022-08-22 00:42:00 +0600

    ok

--- octb/__init__.py
+++ octb/__init__.py
@@ -8,9 +8,9 @@ logging.basicConfig(
 )
 LOGGER = logging.getLogger(__name__)
 
-if sys.version_info[0] < 3 or sys.version_info[1] < 10:
-    LOGGER.error("You MUST have a python version of at least 3.10! Multiple features depend on this. Bot quitting.")
-    quit(1)
+# if sys.version_info[0] < 3 or sys.version_info[1] < 10:
+#     LOGGER.error("You MUST have a python version of at least 3.10! Multiple features depend on this. Bot quitting.")
+#     quit(1)
 
 
 # env
