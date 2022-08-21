f9084ffc36a4c193be49e9ace90323557d6cdf70
indicozy <berkutkarlibai@gmail.com>
2022-08-21 17:06:40 +0600

    ok

--- octb/__main__.py
+++ octb/__main__.py
@@ -10,10 +10,12 @@ from octb.modules import ALL_MODULES
 
 from octb.modules.sql import generator
 
-import importlib
+from decouple import config
+STORAGE = config('STORAGE')
 
 IMPORTED = {}
 
+import importlib
 for module_name in ALL_MODULES:
     imported_module = importlib.import_module("octb.modules." + module_name)
     if not hasattr(imported_module, "__mod_name__"):
@@ -24,7 +26,28 @@ for module_name in ALL_MODULES:
     else:
         raise Exception("Can't have two modules with the same name! Please change one")
 
+import os
+def generate_folders():
+    def generate_folder(location, folders):
+        for key, value in folders.items():
+            path = f"{location}/{key}"
+            if not os.path.exists(path):
+                os.mkdir(path)
+            if type(value) is dict:
+                generate_folder(path,value)
+    folders_all = {
+        "photos": {
+            "product": "product",
+            "temp": {
+                "product": "product",
+            }
+        }
+    }
+    location_root=f"{STORAGE}"
+    generate_folder(location_root, folders_all)
+
 def main():
+    generate_folders()
     application.run_polling()
 
 if __name__ == '__main__':
